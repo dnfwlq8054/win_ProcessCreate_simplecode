ee64bdc487a6cf1f195debb7ae3027e6219f5f98
경환 최 <dnfwlq8054@naver.com>
2019-12-27 22:51:48 +0900

    fix

--- mini_project_console/mini_project_console/main.cpp
+++ mini_project_console/mini_project_console/main.cpp
@@ -3,6 +3,7 @@
 #include <winddi.h>
 #include <string>
 #include <boost/tokenizer.hpp>
+#include <algorithm>
 
 using namespace std;
 typedef basic_string<TCHAR> TSTRING;
@@ -35,7 +36,14 @@ TSTRING cmdTokenList[CMD_TOKEN_NUM];
 bool CmdProcessing(void) {
 	_fputts(_T("Best command prompt>> "), stdout);
 	cin >> cmdString;
-
+	int index = 0;
 	boost::tokenizer<boost::escaped_list_separator<char>> tok(cmdString);
+	for_each(tok.begin(), tok.end(), [&](TSTRING s) {
+		cmdTokenList[index++] = s;
+		});
 
+	for (int i = 0; i < CMD_TOKEN_NUM; i++)
+		cout << cmdTokenList[i] << endl;
+
+	return true;
 }
